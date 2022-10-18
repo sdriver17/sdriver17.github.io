bcefa88c04c1662d41f2ffec34455ce4083ad851
Sam Driver <sdriver17@gmail.com>
2022-10-18 01:03:19 -0400

    general overhaul

--- main.js
+++ main.js
@@ -17,3 +17,31 @@ window.onclick = function(event) {
     }
   }
 }
+
+window.onload = function() {
+  showSlides(1, 0);
+  showSlides(1, 1);
+};
+
+let slideIndex = [1,1];
+let slideId = ["mySlides1", "mySlides2"]
+
+
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
+}
+
+function currentSlide(n, no) {
+  showSlides(slideIndex[no] = n, no);
+}
+
+function showSlides(n, no) {
+  let i;
+  let x = document.getElementsByClassName(slideId[no]);
+  if (n > x.length) {slideIndex[no] = 1}    
+  if (n < 1) {slideIndex[no] = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  x[slideIndex[no]-1].style.display = "block";  
+}
