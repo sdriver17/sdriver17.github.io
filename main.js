b3f4291d0791e7dfb39a2bd9d09ba444474fcdbc
Sam Driver <sdriver17@gmail.com>
2022-10-22 12:12:48 -0400

    major updates

--- main.js
+++ main.js
@@ -18,14 +18,18 @@ window.onclick = function(event) {
   }
 }
 
-window.onload = function() {
-  showSlides(1, 0);
-  showSlides(1, 1);
-};
 
-let slideIndex = [1,1];
-let slideId = ["mySlides1", "mySlides2"]
+let slideIndex = [1,1,1];
+let slideId = ["mySlides1", "mySlides2", "mySlides3"]
 
+function initIndexSlides() {
+  showSlides(1,0);
+  showSlides(1,1);
+}
+
+function initLightingSlides() {
+  showSlides(1,2);
+}
 
 function plusSlides(n, no) {
   showSlides(slideIndex[no] += n, no);
